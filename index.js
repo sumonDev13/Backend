d08932855c8c60974072d10d53a7f302e445605c
sumon <sumonmondal1530765@gmail.com>
2023-05-26 13:21:12 +0600

    server created

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("sumon")
+const express = require("express");
+require('dotenv').config();
+const server = express();
+
+server.use(express.static("STATIC_FOLDER"))
+
+server.get("/homepage",(req,res)=>{
+    res.json({"country":"bangladeshi coder"});
+})
+
+server .listen(process.env.PORT,function(){
+    console.log("Server is running",process.env.PORT);
+})
